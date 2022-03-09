d6c6617ec19b8e3de6e46dda4069f350812ab088
alisari41 <alisari41@outlook.com>
2022-03-09 16:58:02 +0300

    Dtoları otomatik şekilde dönüştürme işlemleri gerçekleşiyor. Mapping işlmerli AutoMapper

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+
+namespace FreeCourse.Services.Catalog.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {//Dtoları otomatik şekilde dönüştürme işlemleri gerçekleşiyor
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+            
+
+            //Diğer Classlar için CreateDto UpdateDto varsa onlarıda dönüştürmeyi unutma
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+        }
+    }
+}
