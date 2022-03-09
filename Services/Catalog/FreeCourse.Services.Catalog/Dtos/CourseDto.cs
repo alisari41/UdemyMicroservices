146ba7a0df13ee990ea56dce7b16c1eaa4fb70a8
alisari41 <alisari41@outlook.com>
2022-03-09 15:29:16 +0300

    Dto nesneleri oluşturuldu. Course için Create ve Update nesneleri oluşturuldu

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CourseDto
+    {//Clietlardan herhangi bir property gizlememe gerek bu şekilde kalabilir.
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }//Identity tarafında string tuttuğum için string belirttim
+        public string Picture { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public FeatureDto Feature { get; set; }//Bire bir ilişki
+        public string CategoryId { get; set; }
+        public CategoryDto Category { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseUpdateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Dtos
+{
+    public class CourseUpdateDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Picture { get; set; }
+        public string UserId { get; set; }//Identity tarafında string tuttuğum için string belirttim
+        public FeatureDto Feature { get; set; }//Bire bir ilişki
+        public string CategoryId { get; set; }
+    }
+}
