4f0cf550541a84475c816f7ea427d281a9124e7e
alisari41 <alisari41@outlook.com>
2022-03-10 22:34:48 +0300

    IdentityResourceOwnerPasswordValidator kullanıcı bilgileir doğru ise tokn verme işlemi tamamlandı. Yani kullanıcı 60 gün benim sayfama bir kere bile girmezse accesstoken zamanı dolmuş olacak

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using System;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 using IdentityServer4;
@@ -20,7 +21,15 @@ namespace FreeCourse.IdentityServer
 
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
-                   {
+                   {//Kullanıcı ile ilgili işlemler
+                       new IdentityResources.Email(),
+                       
+                       //Eğerki email şifre gönderdikten sonra bir token almak istiyorsak mutllaka Id göndermemiz lazım
+                       new IdentityResources.OpenId(),
+
+                       new IdentityResources.Profile(),
+                       new IdentityResource(){Name = "roles",DisplayName = "Roles",Description = "Kullanıcı rolleri",UserClaims = new[]{"role"}}//Rolleride alıyorum
+
 
                    };
 
@@ -43,6 +52,22 @@ namespace FreeCourse.IdentityServer
                     ClientSecrets ={new Secret("secret".Sha256())}, // şifremiz olarak düşünebiliriz
                     AllowedGrantTypes = GrantTypes.ClientCredentials, //İzin tipim
                     AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }// izin verilen scopelar
+                },
+                new Client
+                {
+                //Client bilgilerini giriyorum
+                ClientName = "Asp.Net Core MVC",
+                ClientId = "WebMvcClientForUser",
+                ClientSecrets ={new Secret("secret".Sha256())}, // şifremiz olarak düşünebiliriz
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword, //İzin tipim
+                AllowedScopes = { IdentityServerConstants.StandardScopes.Email,IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.OfflineAccess,"roles"},// izin verilen scopelar
+                AccessTokenLifetime = 1*60*60,//AccesToken'nın ömrünü belirtiyorum. Saniye cinsinden 60*60 yparak 1 saat veriyorum
+                RefreshTokenExpiration = TokenExpiration.Absolute,//Kesin bir tarih veriyorum mesela 60 gün sonra alanamasın
+                AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
+                RefreshTokenUsage = TokenUsage.ReUse//refresh token bir kere mi kullanılsın arka arkaya kullanılsınmı . Tekrar kullanılabilir seçiyoruz
+
+                //Yani kullanıcı 60 gün benim sayfama bir kere bile girmezse accesstoken zamanı dolmuş olacak
                 }
             };
     }
