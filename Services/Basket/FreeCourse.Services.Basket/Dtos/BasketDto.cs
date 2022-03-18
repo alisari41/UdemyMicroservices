32b4657b963f539b508c4e6a4f2e15dce3aa3c79
alisari41 <alisari41@outlook.com>
2022-03-18 13:02:21 +0300

    BasketDto/BasketItemDto bire çok ilişki

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }//Sepet kime ait  
+        public string DiscountCode { get; set; }//İndirim Kodu
+        public List<BasketItemDto> basketItems { get; set; }
+
+        public decimal TotalPrice
+        {
+            get => basketItems.Sum(x => x.Price * x.Quantity);
+        }
+    }
+}
---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; }//Miktar kaç tane olduğu
+
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
